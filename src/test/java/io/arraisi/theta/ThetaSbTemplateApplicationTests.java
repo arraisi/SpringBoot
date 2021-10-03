929d29345b0934782e4679c7bb4eb3eb6c3ef73b
Abdul <abdul.rahman@geekseat.com.au>
2021-10-03 20:55:05 +0700

    first commit

---
+++ src/test/java/io/arraisi/theta/ThetaSbTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package io.arraisi.theta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThetaSbTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
